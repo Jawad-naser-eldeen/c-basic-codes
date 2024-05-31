c24b7d6a9d43702fcb92a8aae3afd3391c77ecb6
Jawad-naser-eldeen <161714719+Jawad-naser-eldeen@users.noreply.github.com>
2024-06-01 00:45:44 +0300

    Add files via upload

---
+++ avg code/avg code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int x,y,z,sum,taksim;
+    printf("Please enter first number:");
+    scanf("%d",&x);
+    printf("Please enter second number:");
+    scanf("%d",&y);
+    printf("Please enter t hirdnumber:");
+    scanf("%d",&z);
+    sum=x+y+z;
+    taksim=sum/3;
+    printf("mid is:%d",taksim);
+return 0;
+}
