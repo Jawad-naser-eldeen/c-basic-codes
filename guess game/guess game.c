023e1e5de1a53f39fbaf40ae119aa7cf98d3ca88
Jawad naser eldeen <161714719+Jawad-naser-eldeen@users.noreply.github.com>
2024-06-04 14:19:05 +0300

    Update guess game.c

--- guess game/guess game.c
+++ guess game/guess game.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 int main()
 {
-    int secret,guess,count;
-    count=0;
-    printf("Enter the secret number:");
-    scanf("%d",&secret);
-    printf("Guess the secret number?");
-    scanf("%d",&guess);
-    count=count++;
-    printf("%d",count);
-    if(secret=guess) 
-        printf("true");
+    int secret,guess,count,fcount=0,tcount=0;
+
+    for(count=0 ; count<3 ; count++){
+    printf("Enter the secret number :");
+    scanf("%d" , &secret);
+    printf("Guess the secret number ?");
+    scanf("%d" , &guess);
+    if(secret==guess){
+        printf("true\n");
+        tcount++;}
     else
-        printf("false");
-    return 0;
+        printf("false\n");
+        fcount++;
+}
+    printf("You tried :%d times\n" , count);
+    printf("True :%d\n" , tcount);
+    printf("False :%d\n" , fcount);
 }
